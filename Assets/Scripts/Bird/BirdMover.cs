58b975654b9f9488333f4031583f02db46f7b057
Dmitry <Dimidril@users.noreply.github.com>
2021-03-08 13:39:35 +0300

    Initial commit

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _tapForce = 10;
+    [SerializeField] private float _roytationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+
+    private Vector3 _startPosition;
+    private Rigidbody2D _rigidbody;
+    private Quaternion _maxRotation;
+    private Quaternion _minRotation;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = Vector2.zero;
+
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+
+        ResetBird();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeScale == 1)
+        {
+            _rigidbody.velocity = new Vector2(_speed, 0);
+            transform.rotation = _maxRotation;
+            _rigidbody.AddForce(Vector2.up * _tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _roytationSpeed * Time.deltaTime);
+    }
+
+    public void ResetBird()
+    {
+        transform.position = _startPosition;
+        transform.rotation = Quaternion.Euler(0,0,0);
+        _rigidbody.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/PipeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : ObjectPool
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _secondBetweenSpawn;
+    [SerializeField] private float _maxSpawnPositionY, _minSpawnPositionY;
+
+    private float _elapsedTime = 0;
+
+    private void Start()
+    {
+        Initialize(_template);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if(_elapsedTime > _secondBetweenSpawn)
+        {
+            if(TryGetObject(out GameObject pipe))
+            {
+                _elapsedTime = 0;
+
+                float spawnedPositionY = Random.Range(_minSpawnPositionY, _maxSpawnPositionY);
+                Vector3 spawnPoint = new Vector3(transform.position.x, spawnedPositionY, transform.position.y);
+                pipe.SetActive(true);
+                pipe.transform.position = spawnPoint;
+
+                DisableObjectAboardScreen();
+            }
+        }
+    }
+}
